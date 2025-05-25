5f1df17080a57ffe8d1ffa51e9ca4efe8d9bfac4
Sanjida Khanam <sanjida.ice.778@gmail.com>
2025-05-25 20:43:51 +0600

    created three tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,23 +1,31 @@
 -- Active: 1747928367168@@127.0.0.1@5432@conservation_db
-CREATE Table rangers(
-    ranger_id INT Unique ,
-    "name" VARCHAR(50),
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
     region VARCHAR(50)
-)
+);
 
-CREATE Table species(
-    species_id INT UNIQUE,
+DROP Table rangers
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50),
     scientific_name VARCHAR(50),
     discovery_date DATE,
     conservation_status VARCHAR(50)
-)
+);
 
-CREATE Table sightings(
-    sighting_id INT UNIQUE,
-    ranger_id INT REFERENCES rangers(ranger_id),
+DROP Table species
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
     species_id INT,
+    ranger_id INT,
+    location VARCHAR(100),
     sighting_time TIMESTAMP,
-    "location" VARCHAR(50),
-    notes VARCHAR(50)
-)
+    notes TEXT,
+    FOREIGN KEY (species_id) REFERENCES species (species_id),
+    FOREIGN KEY (ranger_id) REFERENCES rangers (ranger_id)
+);
+
+DROP Table sightings
